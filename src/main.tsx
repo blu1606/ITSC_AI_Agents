78a62531814f4145e9a390f2194ad61666f5a6a1
blu1606 <dongthanhquandtq@gmail.com>
2026-03-26 21:04:35 +0700

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-);
+    <App />import {StrictMode} from 'react';
+    import {createRoot} from 'react-dom/client';
+    import App from './App.tsx';
+    import './index.css';
+
+    createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+    );
 
   </StrictMode>,
 );
